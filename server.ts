082e6cb69e39516b33d05fa7f1d9c81647ae6d44
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2024-01-25 16:55:20 +0700

    Improved port and hostname setup for Node project

--- server.ts
+++ server.ts
@@ -3,18 +3,26 @@ import dotenv from 'dotenv'
 import testRouter from './routes/test'
 
 // Load ENVs
-dotenv.config({path: './config/config.env'})
+dotenv.config({path: './config/config.env'});
 
+// Parameters
 const app = express();
-const port: number = process.env.PORT === undefined ? 5000 : parseInt(process.env.PORT, 10);
+const environment = process.env.NODE_ENV;
+const isDevelopment = environment === 'development';
+const port: number = isDevelopment ?
+    (process.env.PORT_DEV === undefined ? 5001 : parseInt(process.env.PORT_DEV, 10)) :
+    (process.env.PORT === undefined ? 5000 : parseInt(process.env.PORT, 10));
+const hostname: string = isDevelopment ?
+    (process.env.HOSTNAME_DEV === undefined ? 'localhost' : process.env.HOSTNAME_DEV) :
+    (process.env.HOSTNAME === undefined ? 'localhost' : process.env.HOSTNAME);
 
-app.use('/v1/test', testRouter)
+app.use('/v1/test', testRouter);
 
-const server = app.listen(port, () => {
+const server = app.listen(port, hostname, function () {
     console.log(`Server is running on http://localhost:${port}`)
 });
 
-process.on('unhandledRejection', (error, promise) => {
+process.on('unhandledRejection', function (error, promise) {
     console.log(`Error: ${error}`)
     server.close(() => process.exit(1))
 });
