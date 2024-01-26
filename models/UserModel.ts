7660dbb4afa42a911f5350a1104e467c2b72603c
papongun <papon.gun2@gmail.com>
2024-01-26 14:54:59 +0700

    update user model

--- models/UserModel.ts
+++ models/UserModel.ts
@@ -1,16 +1,24 @@
-import mongoose, { Document, ObjectId } from "mongoose";
+import mongoose, { Document, ObjectId, Types } from "mongoose";
 
 export interface User {
+    userId: ObjectId
     firstName: string;
     lastName: string;
-    phoneNumber: string;
     photoURL?: string;
     gender: 'M' | 'F' | 'O';
+    phoneNumber: string;
+    citizenId?: string;
+    verifyFlag: boolean;
+    bankId?: string;
+    bankAccountNo?: string;
 }
 
 export interface UserDocument extends User, Document {}
 
 const UserSchema = new mongoose.Schema<UserDocument>({
+    userId: {
+        type: Types.ObjectId
+    },
     firstName: {
         type: String,
         required: [true, 'First name is required'],
@@ -20,13 +28,6 @@ const UserSchema = new mongoose.Schema<UserDocument>({
         type: String,
         required: [true, 'Last name is required'],
         maxlength: [255, 'Last name cannot be longer than 255 characters'],
-    },
-    phoneNumber: {
-        type: String,
-        unique: true,
-        required: [true, 'Phone number is required'],
-        // You might want to add additional validation for phone numbers
-        // Example: validate: /^[0-9]{10}$/, // 10 digits only
     },
     photoURL: {
         type: String,
@@ -43,6 +44,36 @@ const UserSchema = new mongoose.Schema<UserDocument>({
         enum: ['M', 'F', 'O'],
         required: [true, 'Gender is required']
     },
+    phoneNumber: {
+        type: String,
+        unique: true,
+        required: [true, 'Phone number is required'],
+        // You might want to add additional validation for phone numbers
+        // Example: validate: /^[0-9]{10}$/, // 10 digits only
+    },
+    citizenId: {
+        type: String,
+        unique: true,
+        maxlength: [13, 'Citizen id cannot be longer than 13 characters'],
+        validate: {
+            validator: function (v: string) {
+                // Add custom validation for URL format
+                return /d{13}/.test(v);
+            },
+            message: 'Invalid citizen ID format',
+        },
+    },
+    verifyFlag: {
+        type: Boolean,
+        required: [true, 'Verify flag is required']
+    },
+    bankId: {
+        type: Types.ObjectId,
+    },
+    bankAccountNo: {
+        type: String,
+        maxlength: [10, 'Bank account cannot be longer than 10 characters']
+    }
 }, {
     timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' },
 });
