72bc35b27144a01b791c18a85281b1ef7c13626a
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2024-01-26 18:40:15 +0700

    Token awaiting password authorization

--- models/AuthModel.ts
+++ models/AuthModel.ts
@@ -1,9 +1,8 @@
-export interface TokenInterface {
-    token: string;
-    expiry: number;
-}
-
 export interface LoginInterface {
     phoneNumber: string;
     password: string;
+}
+
+export const isValidLoginInterface = function (obj: any): obj is LoginInterface {
+    return obj && typeof obj.phoneNumber === 'string' && typeof obj.password === 'string';
 }
