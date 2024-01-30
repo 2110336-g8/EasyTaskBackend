27d2d1a519713913873611b186f0672a4fcaa261
papongun <papon.gun2@gmail.com>
2024-01-30 22:00:36 +0700

    task service

--- services/TasksService.ts
+++ services/TasksService.ts
@@ -1,27 +1,22 @@
-import { Error as MongooseError } from 'mongoose'
-import { TaskModel, ITask, ITaskDocument } from '../models/TaskModel'
-import { TaskValidationError } from '../exceptions/TasksError'
-import { MongoError, MongoServerError } from 'mongodb'
+import { ITaskDocument } from '../models/TaskModel'
+import { TasksRepository } from '../repositories/TasksRepo'
+import { Inject, Service } from 'typedi'
 
+@Service()
 class TaskService {
-    static async createTask(
-        taskData: Omit<ITask, '_id'>,
-    ): Promise<ITaskDocument> {
+    private taskRepository: TasksRepository
+
+    constructor(@Inject() taskRepository: TasksRepository) {
+        this.taskRepository = taskRepository
+    }
+    async createTask(taskData: ITaskDocument): Promise<ITaskDocument> {
         try {
-            const newTask: ITaskDocument = await TaskModel.create(taskData)
-            return newTask
+            const task: ITaskDocument = await this.taskRepository.create(
+                taskData,
+            )
+            return task
         } catch (error) {
-            if (error instanceof MongooseError.ValidationError) {
-                throw new TaskValidationError(error.message)
-            } else if ((error as MongoServerError).code == 11000) {
-                throw new TaskValidationError(
-                    (error as MongoServerError).message,
-                )
-            } else if (error instanceof TaskValidationError) {
-                throw new TaskValidationError(error.message)
-            } else {
-                throw new Error('Unknown Error')
-            }
+            throw error
         }
     }
 }
