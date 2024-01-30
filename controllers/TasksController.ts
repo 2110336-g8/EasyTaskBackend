a2bb30da3225522afd8f68e5a269bdb38c04beee
papongun <papon.gun2@gmail.com>
2024-01-30 12:50:34 +0700

    rm unused import

--- controllers/TasksController.ts
+++ controllers/TasksController.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import TaskService from '../services/TasksService'
 import { TaskValidationError } from '../exceptions/TasksError'
 
--- exceptions/TasksError.ts
+++ exceptions/TasksError.ts
@@ -1,5 +1,3 @@
-import mongoose from 'mongoose'
-
 export class TaskValidationError extends Error {
     constructor(message: string) {
         super(message)
