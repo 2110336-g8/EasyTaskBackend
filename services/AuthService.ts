f1b7a0ee31c135e40fdb68f58d97b92688c2a4e8
papongun <papon.gun2@gmail.com>
2024-01-30 23:12:39 +0700

    auth service

--- services/AuthService.ts
+++ services/AuthService.ts
@@ -1,15 +1,22 @@
 import jwt from 'jsonwebtoken'
 import fs from 'fs'
+import { Inject, Service } from 'typedi'
+import { UsersRepository } from '../repositories/UsersRepo'
 import { ILoginInterface } from '../models/AuthModel'
-import { UserModel } from '../models/UserModel'
 
-const key_pair = {
-    key: fs.readFileSync(`${__dirname}/../config/rs256.key`),
-    pub: fs.readFileSync(`${__dirname}/../config/rs256.key.pub`),
-}
-
+@Service()
 class AuthService {
-    static generateToken(
+    private userRepository: UsersRepository
+    private key_pair = {
+        key: fs.readFileSync(`${__dirname}/../config/rs256.key`),
+        pub: fs.readFileSync(`${__dirname}/../config/rs256.key.pub`),
+    }
+
+    constructor(@Inject() userRepository: UsersRepository) {
+        this.userRepository = userRepository
+    }
+
+    generateToken(
         payload: ILoginInterface,
         sessionMinutes: number = parseInt(process.env.JWT_EXP_MIN || '60'),
     ): string {
@@ -18,28 +25,21 @@ class AuthService {
         const subPayload = {
             phoneNumber: payload.phoneNumber,
         }
-        return jwt.sign(subPayload, key_pair.key, {
+        return jwt.sign(subPayload, this.key_pair.key, {
             expiresIn: expiryTime,
             algorithm: 'RS256',
         })
     }
 
-    static decodeToken(token: string) {
-        return jwt.verify(token, key_pair.pub, {
+    decodeToken(token: string) {
+        return jwt.verify(token, this.key_pair.pub, {
             algorithms: ['RS256'],
         })
     }
 
-    /**
-     * Verify user-password pair with the database
-     * Returns false if no user OR password.
-     *
-     * @param login
-     */
-    static async verifyUser(login: ILoginInterface): Promise<boolean> {
-        const user = await UserModel.findOne({ phoneNumber: login.phoneNumber })
+    async verifyUser(login: ILoginInterface): Promise<boolean> {
+        const user = await this.userRepository.findOne(login.phoneNumber)
 
-        // No user in the database.
         if (!user) {
             return false
         }
