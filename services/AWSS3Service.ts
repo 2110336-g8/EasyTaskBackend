801dd91a5be7df11dcd58c06869311736efc2004
nathathai <nathathaiaungudomsin@gmail.com>
2024-02-01 22:53:13 +0700

    AWSS3Service to class

--- services/AWSS3Service.ts
+++ services/AWSS3Service.ts
@@ -1,71 +1,79 @@
 import { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { Service } from "typedi"; // Assuming you are using a dependency injection library like typedi
 import dotenv from 'dotenv';
 
-dotenv.config({path :'./config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
-const bucketName: string | undefined = process.env.AWS_BUCKET_NAME;
-const region: string | undefined = process.env.AWS_BUCKET_REGION;
-const accessKeyId: string | undefined = process.env.AWS_ACCESS_KEY;
-const secretAccessKey: string | undefined = process.env.AWS_SECRET_ACCESS_KEY;
-
-if (!bucketName || !region || !accessKeyId || !secretAccessKey) {
-  throw new Error("One or more AWS environment variables are missing.");
+interface IAWSS3Service {
+  uploadFile(fileBuffer: Buffer, fileName: string, mimetype: string): Promise<any>;
+  deleteFile(imageKey: string): Promise<any>;
+  getObjectSignedUrl(imageKey: string): Promise<string>;
 }
 
-const s3Client = new S3Client({
-  credentials: {
-    accessKeyId,
-    secretAccessKey,
-  },
-  region,
-});
+@Service()
+export class AWSS3Service implements IAWSS3Service {
+  private s3Client: S3Client;
 
+  constructor() {
+    const bucketName: string | undefined = process.env.AWS_BUCKET_NAME;
+    const region: string | undefined = process.env.AWS_BUCKET_REGION;
+    const accessKeyId: string | undefined = process.env.AWS_ACCESS_KEY;
+    const secretAccessKey: string | undefined = process.env.AWS_SECRET_ACCESS_KEY;
 
-export async function uploadFile(
-    fileBuffer: Buffer,
-    fileName: string,
-    mimetype: string
-  ): Promise<any> {
+    if (!bucketName || !region || !accessKeyId || !secretAccessKey) {
+      throw new Error("One or more AWS environment variables are missing.");
+    }
+
+    this.s3Client = new S3Client({
+      credentials: {
+        accessKeyId,
+        secretAccessKey,
+      },
+      region,
+    });
+  }
+
+  async uploadFile(fileBuffer: Buffer, fileName: string, mimetype: string): Promise<any> {
     const uploadParams = {
-      Bucket: bucketName,
+      Bucket: process.env.AWS_BUCKET_NAME,
       Body: fileBuffer,
       Key: fileName,
       ContentType: mimetype,
     };
-  
-    return s3Client.send(new PutObjectCommand(uploadParams));
+
+    return this.s3Client.send(new PutObjectCommand(uploadParams));
   }
-  
-  export async function deleteFile(imageKey: string): Promise<any> {
+
+  async deleteFile(imageKey: string): Promise<any> {
     const deleteParams = {
-      Bucket: bucketName,
+      Bucket: process.env.AWS_BUCKET_NAME,
       Key: imageKey,
     };
-  
-    return s3Client.send(new DeleteObjectCommand(deleteParams));
+
+    return this.s3Client.send(new DeleteObjectCommand(deleteParams));
   }
-  
-  
-export async function getObjectSignedUrl(imageKey: string): Promise<string> {
+
+  async getObjectSignedUrl(imageKey: string): Promise<string> {
     try {
-        const params = {
-            Bucket: bucketName,
-            Key: imageKey,
-        };
+      const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: imageKey,
+      };
 
-        console.log("URL is requested");
-        
-        const command = new GetObjectCommand(params);
+      console.log("URL is requested");
 
-        const seconds: number = parseInt(process.env.IMAGE_EXPIRE_TIME ?? "3600"); // if there isn't this var in env, it will set to 1 hour
-        const url = await getSignedUrl(s3Client, command, { expiresIn: seconds });
+      const command = new GetObjectCommand(params);
 
-        return url;
+      const seconds: number = parseInt(process.env.IMAGE_EXPIRE_TIME ?? "3600"); // if there isn't this var in env, it will set to 1 hour
+      const url = await getSignedUrl(this.s3Client, command, { expiresIn: seconds });
+
+      return url;
     } catch (error) {
-        console.error(error);
-        throw new Error('Error getting signed URL');
+      console.error(error);
+      throw new Error('Error getting signed URL');
     }
+  }
 }
 
-export default { uploadFile, deleteFile, getObjectSignedUrl };
+export default AWSS3Service;
--- services/ImageService.ts
+++ services/ImageService.ts
@@ -1,24 +1,26 @@
 import { Inject, Service } from 'typedi'
 import { ImageRepository } from '../repositories/ImageRepo'
 import { ImageModel, IImageDocument } from '../models/ImageModel'
+import { IRepository } from '../repositories/BaseRepo';
 import { CannotCreateImageError , CannotGetImageError, CannotDeleteImageError} from '../errors/ImageError'
-import AWSS3Service from './AWSS3Service'
-
+import { AWSS3Service } from './AWSS3Service'
 @Service()
 export class ImageService {
-    private imageRepository: ImageRepository;
+  private imageRepository: IRepository<IImageDocument>;
+  private awsS3Service: AWSS3Service;
 
-    constructor(
-        @Inject() imageRepository: ImageRepository,
-    ) {
-        this.imageRepository = imageRepository;
-    }
-
+  constructor(
+    @Inject(() => ImageRepository) imageRepository: IRepository<IImageDocument>,
+    @Inject(() => AWSS3Service) awsS3Service: AWSS3Service,
+  ) {
+    this.imageRepository = imageRepository;
+    this.awsS3Service = awsS3Service;
+  }
 
     async createImage(ownerId: string, fileBuffer: Buffer, mimeType: string, imageKey: string, purpose: string): Promise<IImageDocument> {
         try {
             // Upload the image to AWS S3
-            await AWSS3Service.uploadFile(fileBuffer, ownerId, mimeType);
+            await this.awsS3Service.uploadFile(fileBuffer, ownerId, mimeType);
             // Save image details to the database
             const imageDoc = await this.imageRepository.create({
                 ownerId: ownerId,
@@ -45,9 +47,9 @@ export class ImageService {
                 throw new Error('Image not found for the given ownerId');
             }
             const key = imageDoc.imageKey;
-            const imageUrl = await AWSS3Service.getObjectSignedUrl(key)
+            const imageUrl = await this.awsS3Service.getObjectSignedUrl(key)
 
-            // const imageUrl = await AWSS3Service.getObjectSignedUrl('65b541c5f264a6557e00f08c.jpg') ////uncomment this to test
+            // const imageUrl = await this.awsS3Service.getObjectSignedUrl('65b541c5f264a6557e00f08c.jpg') ////uncomment this to test
             return imageUrl;
         } catch (error) {
             throw new CannotGetImageError('Can not get the image');
@@ -66,7 +68,7 @@ export class ImageService {
             // Extract imageKey from the found image document
             const key = imageDoc.imageKey;
             // Delete image from AWS S3
-            await AWSS3Service.deleteFile(key);
+            await this.awsS3Service.deleteFile(key);
     
             // Delete image details from the database
             const success = await this.imageRepository.deleteOne(imageDoc.id);
