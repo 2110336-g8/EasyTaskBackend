cb240374cf6a24cfbb997385248fe7ddf261436b
papongun <papon.gun2@gmail.com>
2024-02-13 13:03:22 +0700

    refactor general fake dependencies

---
+++ tests/mocks/FakeModel.ts
@@ -0,0 +1,8 @@
+export const fakeModel = {
+    create: jest.fn(),
+    findById: jest.fn(),
+    findOne: jest.fn(),
+    find: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    deleteOne: jest.fn(),
+};
