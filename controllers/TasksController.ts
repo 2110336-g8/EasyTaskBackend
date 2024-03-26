5d3e8afeb806e60127035595512f35df873d8ac5
Chomchaba Chunakiat <Chomchaba.Chanakiat@gmail.com>
2024-03-27 04:20:42 +0700

    submit task, request revision, accept task

--- controllers/TasksController.ts
+++ controllers/TasksController.ts
@@ -12,6 +12,9 @@ import {
     CannotResponseOfferError,
     CannotStartTaskError,
     CannotDismissTaskError,
+    CannotSubmitTaskError,
+    CannotAcceptTaskError,
+    CannotRequestRevisionError,
 } from '../errors/TaskError';
 import dotenv from 'dotenv';
 dotenv.config({ path: './config/config.env' });
@@ -454,13 +457,21 @@ class TasksController {
                 });
                 return;
             }
-            if (task.status != 'Open') {
+            if (task.endDate < new Date()) {
                 res.status(403).json({
                     success: false,
-                    error: 'Task is not open',
+                    error: 'Task is closed. The application period has ended.',
                 });
                 return;
             }
+            if (task.status != 'Open') {
+                res.status(403).json({
+                    success: false,
+                    error: 'Task is not open for applications. The owner has already started the task.',
+                });
+                return;
+            }
+
             const result = await this.tasksService.applyTask(
                 taskId,
                 req.user._id.toString(),
@@ -714,32 +725,129 @@ class TasksController {
 
     submitTask = async (req: Request, res: Response) => {
         try {
-            res;
-        } catch (error) {
-            res.status(500).json({
-                sucess: false,
-                error: 'Internal Server Error',
+            const taskId = req.params.id;
+            const userId = req.user._id;
+            const task = await this.tasksService.getTaskById(taskId);
+            if (!task) {
+                res.status(404).json({
+                    success: false,
+                    error: 'Task Not Found',
+                });
+                return;
+            }
+            await this.tasksService.submitTask(taskId, userId);
+            res.status(200).json({
+                success: true,
+                message: 'You have successfully submitted work.',
             });
-        }
-    };
-
-    requestRevision = async (req: Request, res: Response) => {
-        try {
         } catch (error) {
-            res.status(500).json({
-                sucess: false,
-                error: 'Internal Server Error',
-            });
+            if (error instanceof CannotSubmitTaskError) {
+                res.status(400).json({
+                    success: false,
+                    error: error.message,
+                });
+            } else if (error instanceof CannotUpdateStateError) {
+                res.status(500).json({
+                    sucess: false,
+                    error: error.message,
+                });
+            } else {
+                res.status(500).json({
+                    sucess: false,
+                    error: 'Internal Server Error',
+                });
+            }
         }
     };
 
     acceptWork = async (req: Request, res: Response) => {
         try {
-        } catch (error) {
-            res.status(500).json({
-                sucess: false,
-                error: 'Internal Server Error',
+            const taskId = req.params.id;
+            const workerId = req.body.employee;
+            const task = await this.tasksService.getTaskById(taskId);
+            if (!task) {
+                res.status(404).json({
+                    success: false,
+                    error: 'Task Not Found',
+                });
+                return;
+            }
+            if (task.customerId.toString() != req.user._id) {
+                res.status(403).json({
+                    success: false,
+                    error: 'You are not allowed to accept work of this task',
+                });
+                return;
+            }
+            const result = await this.tasksService.acceptTask(taskId, workerId);
+            res.status(200).json({
+                success: true,
+                task: result,
             });
+        } catch (error) {
+            if (error instanceof CannotAcceptTaskError) {
+                res.status(400).json({
+                    success: false,
+                    error: error.message,
+                });
+            } else if (error instanceof CannotUpdateStateError) {
+                res.status(500).json({
+                    sucess: false,
+                    error: error.message,
+                });
+            } else {
+                res.status(500).json({
+                    sucess: false,
+                    error: 'Internal Server Error',
+                });
+            }
+        }
+    };
+
+    requestRevision = async (req: Request, res: Response) => {
+        try {
+            const taskId = req.params.id;
+            const workerId = req.body.employee;
+            const task = await this.tasksService.getTaskById(taskId);
+            if (!task) {
+                res.status(404).json({
+                    success: false,
+                    error: 'Task Not Found',
+                });
+                return;
+            }
+            if (task.customerId.toString() != req.user._id) {
+                res.status(403).json({
+                    success: false,
+                    error: 'You are not allowed to request revision of this task',
+                });
+                return;
+            }
+            const result = await this.tasksService.requestRevision(
+                taskId,
+                workerId,
+            );
+            res.status(200).json({
+                success: true,
+                task: result,
+            });
+        } catch (error) {
+            if (error instanceof CannotRequestRevisionError) {
+                res.status(400).json({
+                    success: false,
+                    error: error.message,
+                });
+            } else if (error instanceof CannotUpdateStateError) {
+                res.status(500).json({
+                    sucess: false,
+                    error: error.message,
+                });
+            } else {
+                res.status(500).json({
+                    sucess: false,
+                    error: 'Internal Server Error',
+                });
+            }
         }
     };
 }
