9848e112c0b1c08a83a428eec6235e45b591bcf1
nathathai <nathathaiaungudomsin@gmail.com>
2024-04-13 20:26:39 +0700

    wallet model

---
+++ models/WalletModel.ts
@@ -0,0 +1,40 @@
+import mongoose, { Document, Types, Schema } from 'mongoose';
+
+export interface IWallet {
+    userId: Types.ObjectId;
+    walletAmount: number;
+    paymentHistory: Array<{
+        amount: number;
+        type: 'TopUp' | 'StartTaskPayment' | 'Income' | 'Refund';
+        taskId?: Types.ObjectId | null;
+        timeStamp: Date;
+    }>;
+}
+
+export interface IWalletDocument extends IWallet, Document {}
+
+const WalletSchema = new mongoose.Schema<IWalletDocument>({
+    userId: {
+        type: Schema.Types.ObjectId,
+        required: [true, 'Customer Id is required'],
+        ref: 'User',
+    },
+    walletAmount: { type: Number, default: 0 },
+    paymentHistory: [
+        {
+            amount: { type: Number, required: true },
+            type: {
+                type: String,
+                enum: ['TopUp', 'StartTaskPayment', 'Income', 'Refund'],
+                required: true,
+            },
+            taskId: { type: Types.ObjectId },
+            timeStamp: { type: Date, default: Date.now },
+        },
+    ],
+});
+
+export const WalletModel = mongoose.model<IWalletDocument>(
+    'Wallet',
+    WalletSchema,
+);
