ea7840e7aca028eee9e955e010bc8d5deac88b28
nathathai <nathathaiaungudomsin@gmail.com>
2024-04-20 00:03:36 +0700

    fix transfer

--- controllers/TasksController.ts
+++ controllers/TasksController.ts
@@ -858,11 +858,6 @@ class TasksController {
                 task.wages,
             );
             const result = await this.tasksService.acceptTask(taskId, workerId);
-            const transfer = await this.transferService.acceptTaskPayment(
-                taskId,
-                workerId,
-                task.wages,
-            );
             res.status(200).json({
                 success: true,
                 task: result,
